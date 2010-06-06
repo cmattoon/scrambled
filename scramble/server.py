ef01fe98e63338a0e4f222ff02f3a932dac5a934
Brandon Gilmore <brandon@mg2.org>
2010-06-06 16:47:00 -0700

    it works!

--- scramble/server.py
+++ scramble/server.py
@@ -1,20 +1,25 @@
-import mimetypes
 import optparse
 import os
-import re
 import sys
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 from scramble import __version__
 
-class PyPIHandler(BaseHTTPRequestHandler):
+# HTML templates
+HEADER = """<html><head><title>{title}</title></head><body><h1>{title}</h1>"""
+ITEM   = """<a href="../../package/{package}">{package}</a><br/>"""
+FOOTER = """</body></html>"""
+
+class PyPIHandler(SimpleHTTPRequestHandler):
     server_version = "scrambled/{version}".format(version=__version__)
 
     def do_GET(self):
         if self.path.startswith("/simple/"):
             return self.search()
         elif self.path.startswith("/package/"):
-            return self.fetch()
+            self.path = self.path[9:] # snip prefix
+            return SimpleHTTPRequestHandler.do_GET(self)
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
@@ -23,38 +28,30 @@ class PyPIHandler(BaseHTTPRequestHandler):
             self.wfile.write("Not Found")
 
     def search(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-
-        self.wfile.write("searching...")
-
-    def fetch(self):
-        m = re.match(r"[\./]*([^/]+)$", self.path[9:])
-        if not m:
-            self.send_response(406)
-            self.send_header("Content-type", "text/plain")
+        if self.path[-1] != '/':
+            self.send_response(301)
+            self.send_header("Location", self.path + "/")
             self.end_headers()
-            self.wfile.write("Invalid Request")
             return
+        else:
+            pkgname  = self.path[8:-1]
 
-        pkg = os.path.join(self.server.pkgdir, m.group(1))
-        if not os.path.exists(pkg):
+        packages = filter(lambda f: f.startswith(pkgname), os.listdir(self.server.pkgdir))
+        if not packages:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
+
             self.wfile.write("Not Found")
-            return
+        else:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
 
-        self.send_response(200)
-        self.send_header("Content-type", mimetypes.guess_type(pkg)[0] or 'application/octet-stream')
-        self.send_header("Content-length", os.path.getsize(pkg))
-        self.end_headers()
-
-        self.wfile.write(file(pkg).read())
-
-
-
+            self.wfile.write(HEADER.format(title="Links for " + pkgname))
+            for pkg in packages:
+                self.wfile.write(ITEM.format(package=pkg))
+            self.wfile.write(FOOTER)
 
 
 def run():
@@ -66,17 +63,20 @@ def run():
     parser.add_option("-p", "--port", dest="port", default=8000, type="int",
                       help="port to listen on  (default: %default)")
 
-    options, args = parser.parse_args()
-    if len(args) != 1:
+    opt, arg = parser.parse_args()
+    if len(arg) != 1:
         parser.error("incorrect number of arguments")
-    if not os.path.exists(os.path.join(args[0], ".")):
+    if not os.path.exists(os.path.join(arg[0], ".")):
         parser.error("invalid package_dir")
 
     try:
-        mimetypes.add_type('application/zip', 'egg')
-        server = HTTPServer((options.bind, options.port), PyPIHandler)
-        server.pkgdir = args[0]
+        sys.stderr.write("starting scrambled... [{0}:{1}]\n".format(opt.bind, opt.port))
+        server = HTTPServer((opt.bind, opt.port), PyPIHandler)
+        server.pkgdir = arg[0]
+
+        os.chdir(server.pkgdir)
         server.serve_forever()
+
     except KeyboardInterrupt:
         sys.stderr.write("scrambled shutting down...\n")
         server.socket.close()
--- setup.py
+++ setup.py
@@ -4,8 +4,27 @@ from scramble import __version__
 setup(
     name = "scrambled",
     version = __version__,
-    description = "Python Package Server",
-    long_description = "",
+    description = "a PyPI-compatible package server"
+    long_description = """\
+About
+=====
+
+scrambled is a tiny PyPI-comptable package server that can be used to index
+and serve a directory of Python packages and eggs. 
+
+Overview
+========
+
+* No external dependencies
+* Simple configuration: point it at a directory and run
+* Conforms to PyPI conventions for integration with easy_install, pip, et al
+
+Source
+======
+
+Up-to-date sources can always be found at the `scrambled GoogleCode site
+<http://code.google.com/p/scrambled/>`_.
+"""
     url = "http://code.google.com/p/scrambled",
 
     maintainer = "Brandon Gilmore",
